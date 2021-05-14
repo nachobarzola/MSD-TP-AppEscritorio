1653dc22ea8253cc2811169245d493ca0e434624
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-05-14 10:32:15 -0300

    Initial commit

---
+++ src/main/java/mds/tp/becaalimentaria/App.java
@@ -0,0 +1,13 @@
+package mds.tp.becaalimentaria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
